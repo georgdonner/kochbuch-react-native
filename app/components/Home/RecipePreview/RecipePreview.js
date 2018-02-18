7eb43aa21c7844692ff4f08b7ea68d9167d9adba
Georg Donner <georgdonner98@gmail.com>
2018-02-18 19:34:45 +0100

    Better touch feedback for recipe previews.

--- app/components/Home/RecipePreview/RecipePreview.js
+++ app/components/Home/RecipePreview/RecipePreview.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Image, Text, TouchableOpacity, View } from 'react-native';
+import { Image, Text, TouchableNativeFeedback, View } from 'react-native';
 
 import BottomGradient from '../../common/BottomGradient/BottomGradient';
 import styles from './styles';
@@ -15,27 +15,29 @@ export default (props) => {
   else if (props.recipe.categories.includes('Vegetarisch')) info = 'vegetarisch';
   return (
     <View style={styles.recipe}>
-      <TouchableOpacity
+      <TouchableNativeFeedback
         onPress={() => {
           props.navigator.push({ screen: 'my.Recipe', title: props.recipe.title, passProps: { recipe: props.recipe } });
         }}
       >
-        <View style={styles.image}>
-          <Image
-            source={{
-              uri: props.recipe.heroImage ? props.recipe.heroImage.replace('w:2000', 'w:600') :
-              'http://timgratton.com/wp-content/uploads/2018/01/placeholder-600x400.png',
-            }}
-            style={styles.image}
-          />
-          <BottomGradient />
-          <View style={styles.info}>
-            <Text style={styles.infoText}>{info}</Text>
+        <View>
+          <View style={styles.image}>
+            <Image
+              source={{
+                uri: props.recipe.heroImage ? props.recipe.heroImage.replace('w:2000', 'w:600') :
+                'http://timgratton.com/wp-content/uploads/2018/01/placeholder-600x400.png',
+              }}
+              style={styles.image}
+            />
+            <BottomGradient />
+            <View style={styles.info}>
+              <Text style={styles.infoText}>{info}</Text>
+            </View>
           </View>
+          <Text style={styles.title}>{props.recipe.title}</Text>
+          <View style={styles.categories}>{categories}</View>
         </View>
-        <Text style={styles.title}>{props.recipe.title}</Text>
-        <View style={styles.categories}>{categories}</View>
-      </TouchableOpacity>
+      </TouchableNativeFeedback>
     </View>
   );
 };
